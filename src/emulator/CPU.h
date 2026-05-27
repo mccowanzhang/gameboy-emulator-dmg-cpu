cf81c4ca4cebc30d0c39cf459f3894ad27ad9c9e
McCowan Zhang <mccowan.zhang@gmail.com>
2026-05-27 07:16:39 -0400

    Revert "add op impl in cpu"
    This reverts commit 33df76df53acea51c8404329a6e192b7b21704f2.

--- src/emulator/CPU.h
+++ src/emulator/CPU.h
@@ -11,16 +11,10 @@ public:
     explicit CPU(Memory& memory);
     void Reset();
     void Step();
-    Registers GetRegisters();
 
 private:
     uint8_t Fetch8();
     uint16_t Fetch16();
-
-    enum ArithTarget : uint8_t {
-        A, B, C, D, E, H, L,
-    };
-    void ADD(ArithTarget target);
 
     Memory& memory_;
     Registers registers_;
--- src/tests/CPU.cpp
+++ src/tests/CPU.cpp
@@ -1,40 +1,7 @@
 #include "src/emulator/CPU.h"
 
-#include "src/emulator/memory.h"
-
 #include <gtest/gtest.h>
 
-TEST(CPU, registers) {
-    Memory memory;
-    CPU cpu(memory);
-    Registers registers = cpu.GetRegisters();
-
-    EXPECT_EQ(registers.GetFlag(Flag::Z_FLAG), false);
-    EXPECT_EQ(registers.GetFlag(Flag::N_FLAG), false);
-    EXPECT_EQ(registers.GetFlag(Flag::H_FLAG), false);
-    EXPECT_EQ(registers.GetFlag(Flag::C_FLAG), false);
-    EXPECT_EQ(registers.AF(), 0x0000);
-    EXPECT_EQ(registers.BC(), 0x0000);
-    EXPECT_EQ(registers.DE(), 0x0000);
-    EXPECT_EQ(registers.HL(), 0x0000);
-    EXPECT_EQ(registers.A, 0x00);
-    EXPECT_EQ(registers.B, 0x00);
-    EXPECT_EQ(registers.C, 0x00);
-    EXPECT_EQ(registers.D, 0x00);
-    EXPECT_EQ(registers.E, 0x00);
-    EXPECT_EQ(registers.H, 0x00);
-    EXPECT_EQ(registers.L, 0x00);
-
-    // should not change cpu registers
-    registers.A = 0x01;
-    Registers registers2 = cpu.GetRegisters();
-    EXPECT_EQ(registers2.A, 0x00);
-}
-
-TEST(CPU, ADD) {
-    Memory memory;
-    CPU cpu(memory);
-    
-    // TODO: test ADD
-    Registers registers = cpu.GetRegisters();
+TEST(CPU, reset) {
+    EXPECT_EQ(1 + 1, 2);
 }
