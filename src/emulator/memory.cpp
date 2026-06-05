13beeed4e9cf16f90b370149191cedb7c98da589
McCowan Zhang <mccowan.zhang@gmail.com>
2026-06-04 20:56:47 -0400

    improve logging

--- src/emulator/memory.cpp
+++ src/emulator/memory.cpp
@@ -17,13 +17,13 @@ void Memory::Reset() {
 }
 
 uint8_t Memory::Read8(uint16_t addr) const {
-    LOG(INFO) << "Read: " << std::bitset<8>(memory_[addr])
+    LOG(INFO) << "Read8: " << std::bitset<8>(memory_[addr])
               << " at addr: " << std::hex << addr;
     return memory_[addr];
 }
 
 void Memory::Write8(uint16_t addr, uint8_t val) {
-    LOG(INFO) << "Wrote: " << std::bitset<8>(val)
+    LOG(INFO) << "Wrote8: " << std::bitset<8>(val)
               << " at addr: " << std::hex << addr;
     memory_[addr] = val;
 }
@@ -31,7 +31,7 @@ void Memory::Write8(uint16_t addr, uint8_t val) {
 uint16_t Memory::Read16(uint16_t addr) const {
     uint8_t lsb = memory_[addr];
     uint8_t msb = memory_[addr + 1];
-    LOG(INFO) << "Read: " << std::bitset<16>(Promote(lsb, msb))
+    LOG(INFO) << "Read16: " << std::bitset<16>(Promote(lsb, msb))
               << " at addr: " << std::hex << addr; 
     return Promote(lsb, msb);
 }
@@ -39,7 +39,7 @@ uint16_t Memory::Read16(uint16_t addr) const {
 void Memory::Write16(uint16_t addr, uint16_t val) {
     memory_[addr] = LSB(val);
     memory_[addr + 1] = MSB(val);
-    LOG(INFO) << "Wrote: " << std::bitset<16>(val)
+    LOG(INFO) << "Wrote16: " << std::bitset<16>(val)
               << " at addr: " << std::hex << addr;
 }
 
